42ec7eda0692267f44c5ed22e4335599d4dd6fb0
libsh701 <72676565+libsh701@users.noreply.github.com>
2021-11-13 12:32:32 +0100

    Initial commit

---
+++ loops/loops.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+
+using namespace std; 
+int main() 
+{
+    for ( int i = 0 ;i < 5 ; i++)
+    {
+        cout<< "What up"<<endl;
+    }
+    cout<<"-----------"<<endl;
+
+    for( int i=0 ; i < 9 ; i++)
+    {
+        cout<< i <<endl; 
+    }
+    cout<<"-----------"<<endl; 
+    int i = 0; 
+    while (i < 5 )
+    {
+        cout<<"Sup world"<<endl; 
+        i++;
+    };
+return 0; 
+}
---
+++ Öv1/Helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int x = 20 ;
+    int y = 25 ;
+    int z = 66;
+cout<< x + y + z <<endl; 
+return 0; 
+    
+} 
